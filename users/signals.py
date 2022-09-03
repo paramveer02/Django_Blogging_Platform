2788da38f97084ad1c987dc08da1cd22a264bfab
Paramveer <paramveermarwahafc@gmail.com>
2022-09-03 14:43:48 +0200

    create profile object when user instance is created

---
+++ users/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def profile_create(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def profile_save(sender, instance, **kwargs):
+    instance.profile.save()
