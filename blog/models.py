91d22e76b30f74dd9ed218b762a50a3684bd8f4b
Paramveer <paramveermarwahafc@gmail.com>
2022-09-06 16:25:06 +0200

    added get_absolute_url to redirect user back to the created post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -11,3 +12,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+
+    # to redirect the view, after the post is created and tell django where to find the model url
+    # once an object is created, we want to redirect users to the model detail view
